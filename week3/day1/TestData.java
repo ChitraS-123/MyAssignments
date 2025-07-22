006ba28ae554d35cc273ca94acab9c1813fcb1b4
ChitraS-123 <CHITRA7SAHOO@GMAIL.COM>
2025-07-21 22:41:52 -0500

    Add files via upload

---
+++ week3/day1/TestData.java
@@ -0,0 +1,19 @@
+package week3.day1;
+
+public class TestData {
+	// Superclass
+	
+
+	    // Method 1
+	    public void enterCredentials() {
+	        System.out.println("Entering credentials...");
+	    }
+
+	    // Method 2
+	    public void navigateToHomePage() {
+	        System.out.println("Navigating to home page...");
+	    }
+	}
+
+
+
