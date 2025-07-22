ec03330db16c359505a589715c879b521c2268d1
ChitraS-123 <CHITRA7SAHOO@GMAIL.COM>
2025-07-21 22:30:57 -0500

    Add files via upload
    week3day1

---
+++ week3/day1/InheritanceDemo.java
@@ -0,0 +1,15 @@
+package week3.day1;
+
+public class InheritanceDemo {
+    public static void main(String[] args) {
+        // Object of subclass
+        LoginTestData loginData = new LoginTestData();
+
+        // Call methods
+        loginData.enterCredentials();       // Overridden method with super
+        loginData.navigateToHomePage();     // Inherited directly
+        loginData.enterUsername();          // Subclass method
+        loginData.enterPassword();          // Subclass method
+    }
+}
+
