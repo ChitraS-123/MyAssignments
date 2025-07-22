834a50848c9368e5ed80ccb846791a8bfce90756
ChitraS-123 <CHITRA7SAHOO@GMAIL.COM>
2025-07-21 22:53:11 -0500

    Add files via upload

---
+++ week3/day1/CheckBoxButton.java
@@ -0,0 +1,10 @@
+package week3.day1;
+
+
+class CheckBoxButton extends Button {
+    public void clickCheckButton() {
+        System.out.println("CheckBoxButton clicked");
+    }
+}
+
+
---
+++ week3/day1/RadioButton.java
@@ -0,0 +1,11 @@
+package week3.day1;
+
+
+	class RadioButton extends Button {
+	    public void selectRadioButton() {
+	        System.out.println("RadioButton selected");
+	    }
+	}
+
+
+
