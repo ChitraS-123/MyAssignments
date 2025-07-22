95c89c5317c4255b0c8042e27ed2f0a317a1ee3f
ChitraS-123 <CHITRA7SAHOO@GMAIL.COM>
2025-07-21 22:40:16 -0500

    Add files via upload

---
+++ day1/Main.java
@@ -0,0 +1,16 @@
+package week3.day1;
+
+
+	public class Main {
+	    public static void main(String[] args) {
+	        System.out.println("----Using BasePage----");
+	        BasePage base = new BasePage();
+	        base.performCommonTasks();
+
+	        System.out.println("\n----Using LoginPage----");
+	        LoginPage login = new LoginPage();
+	        login.performCommonTasks();
+	    }
+	}
+
+
