7e985b5b179f0506ee880720b2668a9571491fee
ChitraS-123 <CHITRA7SAHOO@GMAIL.COM>
2025-07-23 09:40:46 -0500

    Delete week3 directory

--- week3/day2/CanaraBank.java
+++
@@ -1,9 +0,0 @@
-package week3.day2;
-
-
-	public abstract class CanaraBank implements Payments {
-	    // Abstract method to be implemented by subclasses
-	    public abstract void recordPaymentDetails();
-	}
-
-
--- week3/day2/MySqlConnection.java
+++
@@ -1,9 +0,0 @@
-package week3.day2;
-
-
-	public abstract class MySqlConnection implements DatabaseConnection {
-	    // New abstract method for subclasses to implement
-	    public abstract void executeQuery();
-	}
-
-
