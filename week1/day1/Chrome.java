ff6553155f7a6998bee4cd43f2afbb683438e617
ChitraS-123 <CHITRA7SAHOO@GMAIL.COM>
2025-07-23 09:31:16 -0500

    Add files via upload

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version=91.0f;
+		String developer="google";
+		boolean beta=false;
+		int releaseYear =2008;
+		char shortcutKey='c';
+		
+		
+		System.out.println("The version is "+(version));
+		System.out.println("The developr is "+(developer));
+		System.out.println("Beta is "+(beta));
+		System.out.println("The releaseYear is "+(releaseYear));
+		System.out.println("The Shortcut is " +(shortcutKey));
+		
+	}
+
+}
